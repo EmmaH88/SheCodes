7807faefd7a690dcd549f117867e981ef0a2cb10
emma hanby <111751257+EmmaH88@users.noreply.github.com>
2022-10-17 11:31:14 +0100

    17/10/22

--- Pro Course/homework/src/script.js
+++ Pro Course/homework/src/script.js
@@ -10,23 +10,7 @@ function formatDate() {
     "Saturday",
   ];
 
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "Decemeber",
-  ];
-
   let day = days[today.getDay()];
-  let month = months[today.getMonth()];
   let date = today.getDate();
   let hours = today.getHours();
   if (hours < 10) {
@@ -36,31 +20,8 @@ function formatDate() {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let li = document.querySelector("li#date");
-  li.innerHTML = ` 🗓️ ${day}, ${hours}:${minutes}`;
+  let h3 = document.querySelector("h3#date");
+  h3.innerHTML = ` 🗓️ ${day}, ${hours}:${minutes}`;
   return date;
 }
 formatDate();
-
-function newCity(event) {
-  event.preventDefault();
-  let inputCity = document.querySelector("#input-city").value;
-  inputCity = inputCity.toLowerCase();
-  inputCity = inputCity.trim();
-  let newCity = document.querySelector("#city");
-  newCity.innerHTML = `🌆${inputCity}`;
-}
-let searchBox = document.querySelector("#search-box");
-searchBox.addEventListener("submit", newCity);
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = temperatureElement.innerHTML = Math.round(
-    (temperature * 9) / 5 + 32
-  );
-}
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
